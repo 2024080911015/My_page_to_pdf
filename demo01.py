6b2af6e0614c4ab803dc39ba7b7ea0f0390ffa57
2024080911015 <13271722058@163.com>
2025-07-02 18:56:34 +0800

    更改

--- demo01.py
+++ demo01.py
@@ -2,7 +2,7 @@ import asyncio
 import time
 import os
 
-from openpyxl.styles.builtins import output
+from openpyxl.styles.builtins import output, title
 from playwright.async_api import async_playwright
 from playwright_stealth import Stealth
 import smtplib
@@ -12,7 +12,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 async def my_page_to_pdf(url:str,output_path:str):
     async with Stealth().use_async(async_playwright()) as p:
-        browse=await p.chromium.launch(headless=False)
+        browse=await p.chromium.launch(headless=True)
         content=await browse.new_context(storage_state="cookies.json")
         page =await content.new_page()
         await page.goto(url,wait_until="networkidle",timeout=60000)
@@ -63,12 +63,24 @@ async def get_favorite_pages(url:str):
             locator=link_locators.nth(i)
             title=await locator.inner_text()
             links=await locator.get_attribute("href")
+            if links.startswith("//"):
+                links="https:"+links
             all_links.append({"title":title,"links":links})
-            print(f"  {i + 1}. {title}")
+        return all_links
 
 async def main():
     time=os.times()
     url="https://www.zhihu.com/collection/967495634"
-    await get_favorite_pages(url)
+    all_links=await get_favorite_pages(url)
+    for i,item in enumerate(all_links):
+        title=item["title"]
+        link=item["links"]
+        output_path = "C:/Users/iiijj/Desktop/My_page_to_pdf/" + str(i) + ".pdf"
+        await my_page_to_pdf(link,output_path)
+        print("已经完成第"+str(i+1)+"个任务")
+    print("完成")
+
+
+
 if __name__=="__main__":
     asyncio.run(main())
